2f1e3e173e696e9df9429554cd4fc0a94143d883
Zach <147861361+1054009@users.noreply.github.com>
2024-04-19 13:26:57 -0400

    Create run_file for SQL

--- src/scripts/db_init.py
+++ src/scripts/db_init.py
@@ -7,44 +7,16 @@ run_query("drop table if exists `students`")
 
 run_query("drop table if exists `assignments`")
 run_query("drop table if exists `assignment_questions`")
+run_query("drop table if exists `assignment_question_options`")
+
+run_query("drop table if exists `assignment_attempts`")
+run_query("drop table if exists `assignment_attempt_responses`")
 
 run_query("set FOREIGN_KEY_CHECKS = 1")
 
 # Initialize tables
-run_query("""
-	create table `users`
-	(
-		`id` int unsigned auto_increment,
-		`first_name` varchar(64) not null,
-		`last_name` varchar(64) not null,
-		`email_address` varchar(64) unique not null,
-		`password` blob not null,
 
-		primary key (`id`)
-	)
-""")
-
-run_query("""
-	create table `teachers`
-	(
-		`id` int unsigned auto_increment,
-		`user_id` int unsigned not null,
-
-		primary key (`id`),
-		foreign key (`user_id`) references `users` (`id`) on delete cascade on update restrict
-	)
-""")
-
-run_query("""
-	create table `students`
-	(
-		`id` int unsigned auto_increment,
-		`user_id` int unsigned not null,
-
-		primary key (`id`),
-		foreign key (`user_id`) references `users` (`id`) on delete cascade on update restrict
-	)
-""")
+run_file("./scripts/db/tables.sql")
 
 # Apply
 sql.commit()
--- src/scripts/util.py
+++ src/scripts/util.py
@@ -1,2 +1,9 @@
 def run_query(query, parameters = None):
 	return sql.execute(text(query), parameters)
+
+def run_file(path, parameters = None):
+	path = (EXECUTING_DIRECTORY / path).resolve()
+
+	file = open(path)
+
+	return run_query(file.read(), parameters)
