69dd965ef6409cadd8f98d00fd78213cf073aed8
Zach <147861361+1054009@users.noreply.github.com>
2024-04-19 13:56:47 -0400

    Load dropper

--- src/scripts/db_init.py
+++ src/scripts/db_init.py
@@ -1,22 +1,4 @@
-# Clear everything out
-run_query("set FOREIGN_KEY_CHECKS = 0")
-
-run_query("drop table if exists `users`")
-run_query("drop table if exists `teachers`")
-run_query("drop table if exists `students`")
-
-run_query("drop table if exists `assignments`")
-run_query("drop table if exists `assignment_questions`")
-run_query("drop table if exists `assignment_question_options`")
-
-run_query("drop table if exists `assignment_attempts`")
-run_query("drop table if exists `assignment_attempt_responses`")
-
-run_query("set FOREIGN_KEY_CHECKS = 1")
-
-# Initialize tables
-
+run_file("./db/drop.sql")
 run_file("./db/tables.sql")
 
-# Apply
 sql.commit()
