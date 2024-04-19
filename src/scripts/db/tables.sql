a0d2531d1683865597bbc8acdcd17a69f68a7091
Zach <147861361+1054009@users.noreply.github.com>
2024-04-19 13:56:36 -0400

    Create the rest of the tables

--- src/scripts/db/tables.sql
+++ src/scripts/db/tables.sql
@@ -26,3 +26,62 @@ create table `students`
 	primary key (`id`),
 	foreign key (`user_id`) references `users` (`id`) on delete cascade on update restrict
 );
+
+create table `assignments`
+(
+	`id` int unsigned auto_increment,
+	`teacher_id` int unsigned not null,
+	`due_date` datetime,
+
+	primary key (`id`),
+	foreign key (`teacher_id`) references `teachers` (`id`) on delete cascade on update restrict
+);
+
+create table `assignment_questions`
+(
+	`id` int unsigned auto_increment,
+	`assignment_id` int unsigned not null,
+	`text` text not null,
+	`points` int unsigned not null,
+
+	primary key (`id`),
+	foreign key (`assignment_id`) references `assignments` (`id`) on delete cascade on update restrict
+);
+
+create table `assignment_question_options`
+(
+	`id` int unsigned auto_increment,
+	`question_id` int unsigned not null,
+	`text` text not null,
+	`is_correct` boolean,
+
+	primary key (`id`),
+	foreign key (`question_id`) references `assignment_questions` (`id`) on delete cascade on update restrict
+);
+
+create table `assignment_attempts`
+(
+	`id` int unsigned auto_increment,
+	`sudent_id` int unsigned not null,
+	`assignment_id` int unsigned not null,
+	`submission_date` datetime not null,
+	`graded` boolean not null,
+
+	primary key (`id`),
+	foreign key (`sudent_id`) references `students` (`id`) on delete cascade on update restrict,
+	foreign key (`assignment_id`) references `assignments` (`id`) on delete cascade on update restrict
+);
+
+create table `assignment_attempt_responses`
+(
+	`id` int unsigned auto_increment,
+	`attempt_id` int unsigned not null,
+	`question_id` int unsigned not null,
+	`option_id` int unsigned not null,
+	`graded` boolean not null,
+
+	primary key (`id`),
+	foreign key (`attempt_id`) references `assignment_attempts` (`id`) on delete cascade on update restrict,
+	foreign key (`question_id`) references `assignment_questions` (`id`) on delete cascade on update restrict,
+	foreign key (`option_id`) references `assignment_question_options` (`id`) on delete cascade on update restrict
+);
