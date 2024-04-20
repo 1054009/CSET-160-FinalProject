065cac377d349b17260907a93197cf833da5265a
Zach <147861361+1054009@users.noreply.github.com>
2024-04-19 23:30:02 -0400

    Add basic user validation functions

--- src/scripts/util.py
+++ src/scripts/util.py
@@ -7,3 +7,23 @@ def run_file(path, parameters = None):
 	file = open(path)
 
 	return run_query(file.read(), parameters)
+
+def user_exists(email_address):
+	data = run_query(f"select * from `users` where `email_address` = {email_address}")
+
+	if not data or not data.first():
+		return False
+
+	if not data["id"] or not data["id"].isnumeric():
+		return False
+
+	return True
+
+def validate_login(email_address, password):
+	if not user_exists(email_address):
+		return False
+
+	password.encode("utf-8")
+	stored_password = run_query(f"select `password` from `users` where `email_address` = {email_address}").first()[0]
+
+	return hashlib.sha256(password.digest()) == stored_password
