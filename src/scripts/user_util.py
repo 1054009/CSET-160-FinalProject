db453fa3b9b3fbd81247fe4b46be68355e2987b2
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-20 10:46:31 -0400

    Add functions for getting id and account type
    Get student or teacher id

--- src/scripts/user_util.py
+++ src/scripts/user_util.py
@@ -5,7 +5,7 @@ def user_exists(email_address):
 	"""
 	:param str email_address: The email associated with a user
 	:return: The user id if the user account exists, False otherwise
-	:rtype: bool
+	:rtype: int if the email exists, bool otherwise
 
 	"""
 	user = get_query_rows(f"select * from `users` where `email_address` = '{email_address}'")
@@ -14,6 +14,57 @@ def user_exists(email_address):
 		return False
 
 	return user[0].id
+
+def get_student_id(user_id):
+	"""
+	:param int user_id:
+	:return: The student id if the student account exists, False otherwise
+	:rtype: int if the user_id exists, bool otherwise
+	"""
+
+	student = get_query_rows(f"select * from `students` where `user_id` = {user_id}")
+
+	if len(student) < 1:
+		return False
+
+	return student[0].id
+
+def get_teacher_id(user_id):
+	"""
+	:param int user_id:
+	:return: The teacher id if the teacher account exists, False otherwise
+	:rtype: int if the user_id exists, bool otherwise
+	"""
+
+	teacher = get_query_rows(f"select * from `teachers` where `user_id` = {user_id}")
+
+	if len(teacher) < 1:
+		return False
+
+	return teacher[0].id
+
+def get_account_type(user_id):
+	"""
+	:param int user_id:
+	:return:
+		"student" if the account type is student
+
+		"teacher" if the account type is teacher
+
+		False otherwise
+	:rtype:
+		str if the account exists
+
+		bool otherwise
+	"""
+
+	if get_student_id(user_id):
+		return "student"
+
+	if get_teacher_id(user_id):
+		return "teacher"
+
+	return False
 
 def validate_login(email_address, password):
 	if not user_exists(email_address):
