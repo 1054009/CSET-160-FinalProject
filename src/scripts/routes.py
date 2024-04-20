f612dfa2a7c251cbb15695f509c0a459a1f0b597
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-20 09:12:49 -0400

    Setting up Accounts page

--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -87,3 +87,19 @@ def login():
 			return render_template("login.html", no_navbar = True, error = "Invalid password")
 	else:
 		return render_template("login.html", no_navbar = True)
+
+@app.route("/accounts/", methods = [ "GET", "POST"])
+def view_accounts():
+	page = request.args.get("page")
+	per_page = request.args.get("per_page")
+
+	accounts, page, per_page, min_page, max_page = get_accounts(page, per_page)
+
+	return render_template(
+		"accounts.html",
+		accounts = accounts,
+		page = page,
+		per_page = per_page,
+		min_page = min_page,
+		max_page = max_page
+	)
