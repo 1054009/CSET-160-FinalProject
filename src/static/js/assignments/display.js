c5804fba2be34ba8fdc8eb4ddb120e124cdb87fc
Zach <147861361+1054009@users.noreply.github.com>
2024-04-22 16:52:01 -0400

    Create import

--- src/static/js/assignments/display.js
+++ src/static/js/assignments/display.js
@@ -9,6 +9,13 @@ assignment_display.create_question_body = () =>
 	return question_body
 }
 
+assignment_display.import = (id, data, questions) =>
+{
+	assignment_display.data.set("id", id)
+	assignment_display.data.set("data", data)
+	assignment_display.data.set("questions", questions)
+}
+
 assignment_display.render_to = (display_element) =>
 {
 	display_element.innerHTML = ""
