dd6f895b9c96c79ae1d21f6d98d4f8fbc93f8df4
Zach <147861361+1054009@users.noreply.github.com>
2024-04-22 17:31:19 -0400

    Import assignment renderer

--- src/static/js/assignments/edit.js
+++ src/static/js/assignments/edit.js
@@ -3,9 +3,11 @@ import { Helper } from "../JSModules/helper.js"
 import { DOMBuilder } from "../JSModules/dom_builder.js"
 
 import { Assignment } from "./assignment.js"
+import { AssignmentRenderer } from "./renderer.js"
 
 // Globals
 const g_Helper = new Helper()
+const g_Renderer = new AssignmentRenderer()
 var g_Assignment = null
 
 // Functions
