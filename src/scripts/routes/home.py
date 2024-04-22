447c0728656ffc732d20ceb0ca14e9b8a93c65f8
Zach <147861361+1054009@users.noreply.github.com>
2024-04-22 12:40:45 -0400

    Use validate_session

--- src/scripts/routes/home.py
+++ src/scripts/routes/home.py
@@ -1,9 +1,8 @@
 @app.route("/")
 @app.route("/home/")
 def home():
-	if not session.get("email_address") or not session.get("user_id"):
+	if not validate_session(session):
 		destroy_session(session)
-
 		return redirect("/login")
 
 	return render_template("home.html", account_type = session.get("account_type"))
