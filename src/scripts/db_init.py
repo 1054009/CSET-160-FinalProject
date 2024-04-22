fe8e03ad37363f20289e09a27a721568677ea655
Zach <147861361+1054009@users.noreply.github.com>
2024-04-22 12:21:28 -0400

    Default test accounts
    s@s.s:s
    t@t.t:t

--- src/scripts/db_init.py
+++ src/scripts/db_init.py
@@ -1,4 +1,42 @@
 run_file("./db/drop.sql")
 run_file("./db/tables.sql")
 
+run_query("""
+	insert into `users` values
+	(
+		NULL,
+		'Student',
+		'Account',
+		's@s.s',
+		'043a718774c572bd8a25adbeb1bfcd5c0256ae11cecf9f9c3f925d0e52beaf89'
+	);
+""")
+
+run_query("""
+	insert into `students` values
+	(
+		NULL,
+		1
+	);
+""")
+
+run_query("""
+	insert into `users` values
+	(
+		NULL,
+		'Teacher',
+		'Account',
+		't@t.t',
+		'e3b98a4da31a127d4bde6e43033f66ba274cab0eb7eb1c70ec41402bf6273dd8'
+	);
+""")
+
+run_query("""
+	insert into `teachers` values
+	(
+		NULL,
+		2
+	);
+""")
+
 sql.commit()
