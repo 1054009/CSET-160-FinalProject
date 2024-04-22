29c7adb9a359be490ee6e096450419551039768c
Zach <147861361+1054009@users.noreply.github.com>
2024-04-22 12:39:11 -0400

    Route grades

--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -1,5 +1,6 @@
 load_file("./routes/accounts.py")
 load_file("./routes/home.py")
+load_file("./routes/grades.py")
 load_file("./routes/login.py")
 load_file("./routes/signup.py")
 load_file("./routes/tasks.py")
