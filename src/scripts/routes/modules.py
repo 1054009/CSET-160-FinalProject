c5dd7ed781e5f995673abbfb10325d0c7bab76e5
Zach <147861361+1054009@users.noreply.github.com>
2024-04-22 20:16:36 -0400

    :trollface:

--- src/scripts/routes/modules.py
+++ src/scripts/routes/modules.py
@@ -3,6 +3,6 @@
 #	:/
 #
 
-@app.route("/static/js/<path:path>")
+@app.route("/static/js/<path:path>") # Potential ../ traversal exploit? Nah :clueless:
 def fix_mime(path):
 	return send_file(f"{SERVER_DIRECTORY}/static/js/{path}", mimetype = "text/javascript")
