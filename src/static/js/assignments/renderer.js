f0a611ad1da10df47f81d50469983c9d3aab98b3
Zach <147861361+1054009@users.noreply.github.com>
2024-04-22 20:41:09 -0400

    Start question renderer

--- src/static/js/assignments/renderer.js
+++ src/static/js/assignments/renderer.js
@@ -1,17 +1,60 @@
 import { Helper } from "../JSModules/helper.js"
 import { DOMBuilder } from "../JSModules/dom_builder.js"
 
-import { Assignment } from "./assignment.js"
+import { QUESTION_TYPE } from "./question.js"
 
 export class AssignmentRenderer
 {
 	constructor()
 	{
 		Helper.assignToObject(this)
+
+		this.m_Builder = new DOMBuilder()
 	}
 
-	renderAssignment(assignment, question, renderTarget)
+	renderOpenEnded(question, renderTarget)
 	{
 
+	}
+
+	renderMultipleChoice(question, renderTarget)
+	{
+
+	}
+
+	renderAssignment(assignment, questionNumber, renderTarget)
+	{
+		const helper = this.getHelper()
+
+		if (!helper.isValidElement(renderTarget))
+			return
+
+		renderTarget.innerHTML = ""
+
+		const question = assignment.getQuestion(questionNumber)
+
+		// Main question
+		this.m_Builder.start(renderTarget)
+		{
+			this.m_Builder.startElement("h3")
+			{
+				this.m_Builder.setProperty("innerHTML", question.getText())
+			}
+			this.m_Builder.endElement()
+		}
+		this.m_Builder.end()
+
+		// The rest
+		switch (question.getType())
+		{
+			default:
+			case QUESTION_TYPE.OPEN_ENDED:
+				this.renderOpenEnded(question, renderTarget)
+				break
+
+			case QUESTION_TYPE.MULTIPLE_CHOICE:
+				this.renderMultipleChoice(question, renderTarget)
+				break
+		}
 	}
 }
