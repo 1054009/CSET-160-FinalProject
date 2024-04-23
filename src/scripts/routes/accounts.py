90c7f01c81168902589ab595783c0dccde0662f8
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-23 13:45:19 -0400

    More account_view stuff

--- src/scripts/routes/accounts.py
+++ src/scripts/routes/accounts.py
@@ -23,22 +23,63 @@ def view_account_info():
 	assignment_data = []
 
 	titles = get_query_rows(f"""
-						select `title` from `assignments`
-						where id in
-							(
-								select `assignment_id`
-								from `assignment_attempts`
-								where `student_id` = {session.get("student_id")}
-							);
-						 """)
+		select `title` from `assignments`
+		where id in
+			(
+				select `assignment_id`
+				from `assignment_attempts`
+				where `student_id` = {session.get("student_id")}
+			);
+		""")
+
 	grades = []
 
-	n = get_query_rows(f"""
+	assignment_attempts_ids = get_query_rows(f"""
+		select 	`id` from `assignment_attempts`
+		where `student_id` = {session.get("student_id")}
+	""")
 
+	for i in range(len(assignment_attempts_ids)):
+		attempt_id = assignment_attempts_ids[i]
 
-					""")
+		grade = 0
 
+		responses_ids = get_query_rows(f"""
+			select `id` from `assignment_attempt_responses`
+			where attempt_id = {attempt_id}
+		""")
 
-	"""
+		for j in range(len(responses_ids)):
+			response_id = responses_ids[j]
 
-	"""
+			points = get_query_rows(f"""
+				select `points`
+				from `assignment_questions` as `aq`
+				where `aq`.`id` =
+				(
+					select `question_id`
+					from `assignment_attempt_responses` as `aar`
+					where `aar`.`id` = {response_id}
+				);
+			""")[0].points
+
+			is_correct = get_query_rows(f"""
+				select `is_correct`
+				from `assignment_question_options` as `aqo`
+				where `aqo`.`id =
+				(
+					select `option_id`
+					from `assignment_attempt_responses` as `aar`
+					where `aar`.`id` = {response_id}
+				);
+			""")[0].is_correct
+
+			if is_correct:
+				grade += points
+
+		grades.append(grade)
+
+	assignment_data.append(titles)
+	assignment_data.append(grades)
+
+	render_template("account_info.html", assignment_data = assignment_data)
