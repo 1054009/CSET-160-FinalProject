613a86c3d3f1362e8d0b1ea01b303227a4701c88
Zach <147861361+1054009@users.noreply.github.com>
2024-04-22 20:18:07 -0400

    Create parser module

---
+++ src/static/js/assignments/parser.js
@@ -0,0 +1,27 @@
+import { Assignment } from "./assignment.js"
+import { Question } from "./question.js"
+import { QuestionOption } from "./question_option.js"
+
+export function assignment_to_js(data)
+{
+	const questions = data.get("questions") || []
+	const questionOptions = data.get("question_options") || {}
+
+	const assignment = new Assignment()
+	assignment.m_Questions = questions.map((rawQuestion) =>
+	{
+		const question = new Question(rawQuestion)
+
+		const options = []
+		const rawOptions = questionOptions[question.getID()]
+
+		if (rawOptions)
+			options = rawOptions.map(option => new QuestionOption(option))
+
+		question.m_Options = options
+
+		return question
+	})
+
+	return assignment
+}
