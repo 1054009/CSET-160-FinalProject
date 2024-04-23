3209bd6712c76019fd049bc04af0d9141edf824c
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-23 14:35:06 -0400

    Add function to calculate grade for  attempt

--- src/scripts/functions.py
+++ src/scripts/functions.py
@@ -27,3 +27,49 @@ def get_data(table, page = 1, per_page = 10):
 	data = get_query_rows(f"select * from `{table}` limit {per_page} offset {(page - 1) * per_page}")
 
 	return data, page, per_page, min_page, max_page
+
+def get_points(response_id):
+	return get_query_rows(f"""
+		select `points`
+		from `assignment_questions` as `aq`
+		where `aq`.`id` =
+		(
+			select `question_id`
+			from `assignment_attempt_responses` as `aar`
+			where `aar`.`id` = {response_id}
+		);
+	""")[0].points
+
+def get_is_correct(response_id):
+	return get_query_rows(f"""
+		select `is_correct`
+		from `assignment_question_options` as `aqo`
+		where `aqo`.`id =
+		(
+			select `option_id`
+			from `assignment_attempt_responses` as `aar`
+			where `aar`.`id` = {response_id}
+		);
+	""")[0].is_correct
+
+def get_grade(attempt_id):
+	grade = 0
+
+	# All the responses_ids for the attempt_id
+	responses_ids = get_query_rows(f"""
+		select `id` from `assignment_attempt_responses`
+		where attempt_id = {attempt_id}
+	""")
+
+
+	for j in range(len(responses_ids)):
+		response_id = responses_ids[j]
+
+		points = get_points(response_id)
+
+		is_correct = get_is_correct(response_id)
+
+		if is_correct:
+			grade += points
+
+	return grade
