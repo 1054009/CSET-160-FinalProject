e24a30600a459af3590eabace16c6d07059f218a
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-24 14:58:59 -0400

    Add route to all assignments

--- src/scripts/routes/assignments.py
+++ src/scripts/routes/assignments.py
@@ -1,3 +1,23 @@
+@app.route("/assignments/", methods = [ "GET"])
+def view_assignments():
+	if not validate_session(session):
+		destroy_session(session)
+		return redirect("/login")
+
+	page = request.args.get("page")
+	per_page = request.args.get("per_page")
+
+	assignments, page, per_page, min_page, max_page = get_data('assignments', page, per_page)
+
+	return render_template(
+		"assignments.html",
+		assignments = assignments,
+		page = page,
+		per_page = per_page,
+		min_page = min_page,
+		max_page = max_page
+	)
+
 @app.route("/assignments/add/")
 def initialize_assignment():
 	if not validate_session(session):
