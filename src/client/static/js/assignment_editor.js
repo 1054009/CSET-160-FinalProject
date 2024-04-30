07e8b2d399fca199c2c131be6bc8e054d1f85dde
Zach <147861361+1054009@users.noreply.github.com>
2024-04-30 15:24:34 -0400

    Test editor fetch

--- src/client/static/js/assignment_editor.js
+++ src/client/static/js/assignment_editor.js
@@ -1,8 +1,13 @@
 import { Helper } from "./JSModules/helper.js"
 
+import { Assignment, AssignmentQuestion } from "./assignment_models.js"
+import { AssignmentRenderer } from "./assignment_renderer.js"
+
 const g_Helper = new Helper()
 
 g_Helper.hookEvent(window, "load", false, () =>
 {
-	console.log("hey")
+	const test = new Assignment(1)
+
+	test.fetchQuestions()
 })
