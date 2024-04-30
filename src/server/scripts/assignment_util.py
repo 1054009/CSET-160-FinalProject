7ae862f662b472ae91643c50de9f292f5b271d44
Zach <147861361+1054009@users.noreply.github.com>
2024-04-30 15:24:15 -0400

    More constructors and getters for assignment_util

--- src/server/scripts/assignment_util.py
+++ src/server/scripts/assignment_util.py
@@ -1,4 +1,4 @@
-from models import Assignment
+from models import Assignment, Question, Option
 from session import database
 
 from datetime import datetime
@@ -21,10 +21,69 @@ def create_assignment(creator_id, title, due_date):
 	except:
 		return None
 
+def create_question(assignment_id, text, points, type):
+	try:
+		assignment = get_assignment(assignment_id)
+		if assignment is None:
+			return None
+
+		new_question = Question(
+			assignment_id = assignment_id,
+			text = text,
+			points = points,
+			type = type
+		)
+
+		database.add(new_question)
+		database.flush()
+
+		assignment.questions.append(new_question)
+
+		return new_question
+	except:
+		return None
+
+def create_option(question_id, text, is_correct):
+	try:
+		question = get_question(question_id)
+		if question is None:
+			return None
+
+		new_option = Option(
+			question_id = question_id,
+			text = text,
+			is_correct = is_correct
+		)
+
+		database.add(new_option)
+		database.flush()
+
+		question.options.append(new_option)
+
+		return new_option
+	except:
+		return None
+
 def get_assignment(id):
 	try:
 		assignments = database.query(Assignment)
 
 		return assignments.filter(Assignment.id == id).first()
+	except:
+		return None
+
+def get_question(id):
+	try:
+		questions = database.query(Question)
+
+		return questions.filter(Question.id == id).first()
+	except:
+		return None
+
+def get_option(id):
+	try:
+		options = database.query(Option)
+
+		return options.filter(Option.id == id).first()
 	except:
 		return None
