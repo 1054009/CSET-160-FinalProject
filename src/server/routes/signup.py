44004a42c85d4fc7d0d94486edc69d554a514e45
Zach <147861361+1054009@users.noreply.github.com>
2024-04-30 13:57:47 -0400

    Add receiver for sign up

--- src/server/routes/signup.py
+++ src/server/routes/signup.py
@@ -1,6 +1,38 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
+
+from scripts.password_util import sha_string
+from scripts.user_util import create_user, get_user
 
 @app.route("/signup/")
-def signup():
+def signup_get():
 	return render_template("signup.html")
+
+@app.route("/signup/", methods = [ "POST" ])
+def signup_post():
+	existing = get_user(request.form.get("email", "idiot@gmail.com"))
+	if existing is not None:
+		return render_template(
+			"signup.html",
+
+			error_msg = "User with email already exists"
+		)
+
+	password = request.form.get("password", "nice one")
+	password_verify = request.form.get("password_verify", "nice one")
+
+	if password != password_verify:
+		return render_template(
+			"signup.html",
+
+			error_msg = "Passwords don't match"
+		)
+
+	new_user = create_user(
+		request.form.get("first_name", "nice name"),
+		request.form.get("last_name", "nice name"),
+		request.form.get("email", "idiot@gmail.com"),
+		sha_string(password)
+	)
+
+	return redirect("/home/")
