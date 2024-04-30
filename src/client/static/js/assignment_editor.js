b94eb8915b3435aa1742672a869909595a2f118d
Zach <147861361+1054009@users.noreply.github.com>
2024-04-30 19:13:32 -0400

    Add functionality to previous and next question buttons

--- src/client/static/js/assignment_editor.js
+++ src/client/static/js/assignment_editor.js
@@ -6,13 +6,31 @@ import { AssignmentRenderer } from "./assignment_renderer.js"
 const g_Helper = new Helper()
 const g_Renderer = new AssignmentRenderer()
 
+function render()
+{
+	g_Renderer.render(document.querySelector("#assignment_render_target"), true)
+}
+
 g_Helper.hookEvent(window, "load", false, () =>
 {
+	// Setup controls
+	g_Helper.hookElementEvent(document.querySelector("#btn_prev_question"), "click", true, () =>
+	{
+		g_Renderer.setQuestionNumber(g_Renderer.getQuestionNumber() - 1)
+		render()
+	})
+
+	g_Helper.hookElementEvent(document.querySelector("#btn_next_question"), "click", true, () =>
+	{
+		g_Renderer.setQuestionNumber(g_Renderer.getQuestionNumber() + 1)
+		render()
+	})
+
 	const assignment = new Assignment(ASSIGNMENT_ID)
 
 	assignment.fetchQuestions(() =>
 	{
 		g_Renderer.setAssignment(assignment)
-		g_Renderer.render(document.querySelector("#assignment_render_target"), true)
+		render()
 	})
 })
