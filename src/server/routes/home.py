a53a5cb1bd2f8c522dda6d1b517d1b9548a2cd65
Zach <147861361+1054009@users.noreply.github.com>
2024-04-30 14:05:50 -0400

    Add validate_session on home page

--- src/server/routes/home.py
+++ src/server/routes/home.py
@@ -1,7 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, session, redirect
+
+from scripts.session_util import validate_session
 
 @app.route("/")
 @app.route("/home/")
 def home():
+	if not validate_session(session):
+		return redirect("/login/")
+
 	return render_template("base.html")
