8e86a5e3cc66586c4686bf16420a9fab03dc557b
Zach <147861361+1054009@users.noreply.github.com>
2024-05-01 14:11:48 -0400

    Pass assignments correctly

--- src/server/routes/home.py
+++ src/server/routes/home.py
@@ -1,10 +1,12 @@
 from session import database
 
+import json
 from app import app
 from flask import render_template, session, redirect
 
 from models import Assignment
 from scripts.session_util import validate_session
+from scripts.assignment_util import to_json
 
 @app.route("/")
 @app.route("/home/")
@@ -13,7 +15,10 @@ def home():
 		return redirect("/login/")
 
 	assignments = database.query(Assignment).all()
-	assignments_json = str(assignments)
+	assignment_list = []
+
+	for assignment in assignments:
+		assignment_list.append(to_json(assignment.id))
 
 	# TODO: Check for already taken assignments
 
@@ -22,5 +27,5 @@ def home():
 
 		account_email = session.get("email_address"),
 		account_type = session.get("account_type"),
-		blarg = assignments_json
+		assignment_list = json.dumps(assignment_list)
 	)
