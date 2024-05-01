84521f54de86edf1e8800706f8d2030a75c4c565
Zach <147861361+1054009@users.noreply.github.com>
2024-05-01 13:35:55 -0400

    Test sending through assignments

--- src/server/routes/home.py
+++ src/server/routes/home.py
@@ -1,6 +1,9 @@
+from session import database
+
 from app import app
 from flask import render_template, session, redirect
 
+from models import Assignment
 from scripts.session_util import validate_session
 
 @app.route("/")
@@ -9,9 +12,15 @@ def home():
 	if not validate_session(session):
 		return redirect("/login/")
 
+	assignments = database.query(Assignment).all()
+	assignments_json = str(assignments)
+
+	# TODO: Check for already taken assignments
+
 	return render_template(
 		"home.html",
 
 		account_email = session.get("email_address"),
-		account_type = session.get("account_type")
+		account_type = session.get("account_type"),
+		blarg = assignments_json
 	)
